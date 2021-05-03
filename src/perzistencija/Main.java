7b563dea6c7f7e32fa17b5c91737110badba445e
DejanJovanovic021 <dejan.jovanovic.java021@gmail.com>
2021-05-03 12:30:04 +0200

    Java Swing, MySQL, Persistence

---
+++ src/perzistencija/Main.java
@@ -0,0 +1,14 @@
+
+package perzistencija;
+
+
+public class Main {
+
+    
+    public static void main(String[] args) {
+        
+        Application app = new Application();
+        app.setVisible(true);
+    }
+    
+}
